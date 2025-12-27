d189323efdf6cff17f6a5b57866ff43ac02b341f
Sophomore <17792626+WSQS@users.noreply.github.com>
2025-12-27 20:12:13 +0800

    fix:move file

---
+++ adder/component.cpp
@@ -0,0 +1,18 @@
+#include "adder_cpp.h"
+
+namespace exports
+{
+    namespace docs
+    {
+        namespace adder
+        {
+            namespace add
+            {
+                uint32_t Add(uint32_t x, uint32_t y)
+                {
+                    return x + y;
+                }
+            }
+        }
+    }
+}
