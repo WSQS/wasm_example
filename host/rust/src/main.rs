3cd700487d36ab12d6b3ec80714eaa0a10771ee0
Sophomore <17792626+WSQS@users.noreply.github.com>
2025-12-27 19:56:57 +0800

    fix: remove lib async-std

--- host/rust/src/main.rs
+++ host/rust/src/main.rs
@@ -1,17 +1,13 @@
 use std::path::PathBuf;
 
-mod async_add;
 mod state;
 mod sync_add;
 
-#[async_std::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     let x = 1;
     let y = 2;
     let component: PathBuf = "../../adder/adder.wasm".into();
-    let sum1 = async_add::add(component.clone(), x, y).await?;
-    let sum2 = sync_add::add(component, x, y)?;
-    assert_eq!(sum1, sum2);
-    println!("{} + {} = {sum1}", x, y);
+    let sum = sync_add::add(component, x, y)?;
+    println!("{} + {} = {sum}", x, y);
     Ok(())
 }
