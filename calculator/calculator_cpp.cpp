1f72ead023eec197ae013b79d0e0f72ca6039d90
Sophomore <17792626+WSQS@users.noreply.github.com>
2025-12-28 18:13:07 +0800

    fix: add calculator

---
+++ calculator/calculator_cpp.cpp
@@ -0,0 +1,12 @@
+#include"calculator_cpp.h"
+namespace exports {
+  namespace docs {
+    namespace calculator {
+      namespace calculate {
+        uint32_t EvalExpression(wit::string expr){
+            return 12;
+        }
+      }
+    }
+  }
+}
